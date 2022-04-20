b53968b15f8cb8f64fbbbfed5a42edc3227bab0f
teenza <foxtoface@gmail.com>
2022-04-20 18:07:48 +0700

    0.2

--- TestSCG/test/Test1.swift
+++ TestSCG/test/Test1.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class Test1{
+    
+    var data1:[Int] = [1,3,5,7,9]
+    
+    func run(){
+       
+    }
+    
+    func conv(_ txt:String){
+       
+    }
+}
--- TestSCG/test/Test2.swift
+++ TestSCG/test/Test2.swift
@@ -9,16 +9,29 @@ import Foundation
 
 class Test2{
     
-    var text:[String] = ["aka","Level","hello"]
+    var text:[String] = ["aka","Level","hello","kak","1234","222"]
     
     func run(){
-        
+        for t in text{
+            self.conv(t)
+        }
     }
     
     func conv(_ txt:String){
-        let _txt = txt.components(separatedBy: " ")
-        for i in _txt{
-            print(i)
+        
+        let text = txt;
+        let text_check = text.lowercased()
+        var pdtext = ""
+        let _txt = String(text_check)
+        
+        for i in _txt.reversed(){
+            pdtext = "\(pdtext)\(i)"
+        }
+        
+        if(text_check == pdtext ){
+            print("\(text) is a palindrome")
+        }else{
+            print("\(text) isn’t a palindrome")
         }
     }
 }
