6a7a5583b56312eade1ca886b115536269a7de62
teenza <foxtoface@gmail.com>
2022-04-22 08:45:08 +0700

    0.4

--- TestSCGTests/TestSCGTests.swift
+++ TestSCGTests/TestSCGTests.swift
@@ -9,6 +9,34 @@ import XCTest
 @testable import TestSCG
 
 class TestSCGTests: XCTestCase {
+    
+    let apph = AppHelper()
+    
+    func test_get_json(){
+        apph.getJSON("everything?q=apple&from=2022-04-19&to=2022-04-19&sortBy=popularity") { data in
+            XCTAssertTrue(true)
+        } failure: { error in
+            XCTAssertFalse(false)
+        }
+
+    }
+    
+    func test_setImg(){
+        let _url = URL(string:"https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/d936518a90f42db2b3327639efecbc86.jpg")
+            // Create Data Task
+        let dataTask = URLSession.shared.dataTask(with: _url!) {(data, _, _) in
+                if let data = data {
+                    DispatchQueue.main.async {
+                        XCTAssertTrue(true)
+                    }
+                }else{
+                    XCTAssertFalse(false)
+                }
+            }
+            //Start Data Task
+            dataTask.resume()
+    }
+   
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
---
+++ TestSCGTests/test_test.swift
@@ -0,0 +1,8 @@
+//
+//  test_test.swift
+//  TestSCGTests
+//
+//  Created by kk on 22/4/2565 BE.
+//
+
+import Foundation
